9cde3ffdd971e53c504ee8ef49bacb3a9d92a20f
Roeck <rons9n@gmail.com>
2020-11-10 17:42:32 -0500

    set up axios and api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React,{useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import axios from 'axios';
 
 function App() {
+  const [pokedex,setPokedex]=useState([]);
+  const [wildPokemon,setWildPokemon]=useState({});
+
+  useEffect(() => {
+    encounterWildPokemon()
+  },[])
+
+  const encounterWildPokemon=() => {
+    axios.get('https://pokeapi.co/api/v1/pokemon/151');
+  }
+
   return (
     <div className="app-wrapper">
       <h1>App</h1>
